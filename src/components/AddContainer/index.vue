feca318c9a2244dd32c689d13e4268baa6209c0c
efrat <efrat@yad2.co.il>
2019-02-10 15:38:27 +0200

    remove unneeded

--- src/components/AddContainer/index.vue
+++
@@ -1,4 +0,0 @@
-<template src="./AddContainer.html"></template>
-<script src="./AddContainer.js"></script>
-<style src="./AddContainer.css" scoped lang="css"></style>
-
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,11 +1,7 @@
 /* eslint-disable */
-import axios from 'axios'
-import AddContainer from '../AddContainer/index.vue'
 export default {
   name: 'dashboard',
-  components: {
-      AddContainer
-  },
+  components: {  },
   props: [],
   data () {
     return {
@@ -14,19 +10,6 @@ export default {
   },
   computed: {
 
-  },
-  mounted () {
-    axios.get('http://0.0.0.0:8181/container').then(res => {
-      console.log('res.data:',res.data);
-      if(Array.isArray(res.data)){
-          res.data.forEach(container=>{
-            container.name=container.name.split('"').join('').split('/').join('').split('\\').join('');
-            container.port=JSON.parse(container.port);
-            container.volume=JSON.parse(container.volume);
-          })
-      }
-      this.containers = res.data
-    })
   },
   methods: {
 
